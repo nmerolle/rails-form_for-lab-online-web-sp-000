934e2428eb16402d76500e2eed33235aaf830593
nmerolle <nmerolle11@hotmail.com>
2021-05-15 06:34:03 -0400

    commit tree 4165a8b864df0774960a05eaa1bbc7447fa1674c

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,41 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+    @school_classes = SchoolClass.all
+  end
+
+  def show
+    find_school_class
+  end
+
+  def new
+    @school_class = SchoolClass.new
+  end
+
+  def create
+    @school_class = SchoolClass.create(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  def edit
+    find_school_class
+  end
+
+  def update
+    find_school_class
+    @school_class.update(school_class_params)
+    redirect_to school_class_path(@school_class)
+  end
+
+  private
+
+  def school_class_params
+    params.require(:school_class).permit(:title, :room_number)
+  end
+
+  def find_school_class
+    @school_class = SchoolClass.find(params[:id])
+  end
+end
+
+
